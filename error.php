b1fb536f5c26c8eb0d80c4630601c4706e906f1e
tomas <tomas1234567891016@gmail.com>
2025-06-28 21:22:52 -0300

    Nuevo sistema de error

---
+++ error.php
@@ -0,0 +1,38 @@
+<?php
+    $errores = [
+        "No hay contenido en esta página.",
+        "No se ha encontrado el contenido solicitado."        
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subir</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="js/archivos.js" defer></script>
+</head>
+<body>
+    <nav>
+        <h1>test</h1>
+        <ul>
+            <li><a href="galeria.php?pag=1">Galería</a></li>
+            <li><a href="#">Subir</a></li>
+        </ul>
+        <div class="nav-cuenta">
+            <a href="php/cuenta.php" id="cuenta">Invitado</a>
+        </div>
+    </nav>
+    <header>
+        <h1>Ups, hubo un problema</h1>
+        <?php
+            if ((isset($_GET["id"])) && (is_numeric($_GET["id"]) && ($_GET["id"] > 0) && ($_GET["id"] < (count($errores) + 1)))) {
+                echo "<p id='error'>" . $errores[$_GET["id"] - 1] . "</p>";
+            }
+        ?>
+        <p id="disculpas">Pedimos disculpas.</p>
+    </header>
+</body>
+</html>
