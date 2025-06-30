acb8ae64d8edd17271c4b4c222106a81a03399eb
tomas <tomas1234567891016@gmail.com>
2025-06-29 22:51:25 -0300

    Nueva redirección

--- galeria.php
+++ galeria.php
@@ -26,7 +26,7 @@
         <h1>test</h1>
         <ul>
             <li><a href="#">Galería</a></li>
-            <li><a href="subir.html">Subir</a></li>
+            <li><a href="subir.php">Subir</a></li>
         </ul>
         <div class="nav-cuenta">
             <a href="php/cuenta.php" id="cuenta">Invitado</a>
