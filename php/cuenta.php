2f4872dc95c639debbfe288ca191ecc736133575
tomas <tomas1234567891016@gmail.com>
2025-07-03 14:55:59 -0300

    session_start()

--- php/cuenta.php
+++ php/cuenta.php
@@ -2,6 +2,12 @@
     // Esto manejaría el tema de las sesiones
     // Si no hay una cuenta, entonces lo redirigimos a login
     // Si hay, entonces lo redirigimos al perfil de la cuenta
-    header("Location: ../login.php");
+    session_start();
+    if (isset($_SESSION["logueado"])){
+        header("Location: ../perfil.php");
+    }
+    else{
+        header("Location: ../login.php");
+    }
     exit();
 ?>
