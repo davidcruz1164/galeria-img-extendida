72cceda633f9d1e79834365879f7d701ea354ea5
davidcruz1164 <elreybarbarodav196@gmail.com>
2025-07-10 16:44:42 -0300

    Script

--- js/post/comentar.js
+++ js/post/comentar.js
@@ -1,8 +1,6 @@
 let textarea = document.getElementById("post-comentarios-textarea");
 let enviar = document.getElementById("post-comentarios-enviar");
 
-textarea.value = "asfasfasf"; // Clear the textarea on page load
-
 textarea.addEventListener("input", (e) => {
     if (textarea.value.trim() !== "") {
         enviar.disabled = false;
